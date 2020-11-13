041b6b56a2a5ea99646a70b41f7ceb66971e4ea9
Sergi Grau <sergi.grau@fje.edu>
2020-11-13 13:11:44 +0100

    update progress XHR2

---
+++ M08_indexServidorProgres.js
@@ -0,0 +1,18 @@
+/*
+ * arxiu principal que arrenca el servidor HTTP
+ * @author  sergi.grau@fje.edu
+ * @version 1.0
+ * date 13.11.20
+ * format del document UTF-8
+ *
+ * CHANGELOG
+ * date 13.11.20
+ * - arxiu principal que arrenca el servidor HTTP
+ *
+ * NOTES
+ * ORIGEN
+ * Desenvolupament Aplicacions Web. Jesuïtes el Clot
+ */
+var server = require("./M08_servidorProgres");
+
+server.iniciar();
