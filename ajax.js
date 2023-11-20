98e54aa0536756b73b45c4e063987104bf92f4b9
sergi grau <sergi.grau@fje.edu>
2023-11-20 19:49:36 +0100

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -3,7 +3,8 @@ let xhr;
 function inici() {
     document.getElementById('carregar').addEventListener('click', obtenirProductes);
     document.getElementById('afegir').addEventListener('click', afegirProducte);
-
+    document.getElementById('modificar').addEventListener('click', modificarProducte);
+    document.getElementById('esborrar').addEventListener('click', esborrarProducte);
     xhr = new XMLHttpRequest();
 
 }
@@ -20,8 +21,11 @@ function obtenirProductes() {
 }
 
 function afegirProducte() {
-    // WARNING: For POST requests, body is set to null by browsers.
-    var data = "cat=ORD&nom=Macbook&preu=1700";
+    let cat = document.getElementById('cat').value;
+    let nom = document.getElementById('nom').value;
+    let preu = document.getElementById('preu').value;
+
+    let data = `cat=${cat}&nom=${nom}&preu=${preu}`;
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
@@ -32,6 +36,41 @@ function afegirProducte() {
     xhr.open("POST", "http://localhost:3000/api/productes");
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send(data);
+}
+
+
+function modificarProducte() {
+    let cat = document.getElementById('cat').value;
+    let nom = document.getElementById('nom').value;
+    let preu = document.getElementById('preu').value;
+
+    let data = `cat=${cat}&nom=${nom}&preu=${preu}`;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log('producte modificat');
+        }
+    });
+
+    xhr.open("PUT", `http://localhost:3000/api/productes/${cat}/${nom}`);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+}
+
+function esborrarProducte() {
+    let cat = document.getElementById('cat').value;
+    let nom = document.getElementById('nom').value;
+    let preu = document.getElementById('preu').value;
+
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log('producte esborrar');
+        }
+    });
+
+    xhr.open("DELETE", `http://localhost:3000/api/productes/${cat}/${preu}`);
+    xhr.send(null);
 }
 
 window.addEventListener("load", inici, true);
